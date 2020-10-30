38a17caa8acee92b066246421b5398a8cdf6b74d
Joshua Kang <forkjerkjork@gmail.com>
2020-10-30 08:46:01 +0100

    dAass

--- client/src/components/FrontPage.js
+++ client/src/components/FrontPage.js
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 import { destinationApi, departureApi } from "../Api";
 import { IdContext } from "./IdContext";
 import { useHistory } from "react-router-dom";
-import { FrontPageMain, SearchFlightButton, FrontPageH2, LoadingScreenDiv } from "./Styled";
+import { FrontPageMain, SearchFlightButton, FrontPageH2,LoadingScreenDiv} from "./Styled";
 import LoadingPage from "./Loading+NoResult/LoadingPage"
 
 function FrontPage() {
@@ -15,19 +15,18 @@ function FrontPage() {
   const timeout = useRef(null);
   const { setDestinationId, setDepartureId } = useContext(IdContext);
   const { setDestinationName, setDepartureName } = useContext(IdContext);
-  const { setDestinationCountry, setDepartureCountry } = useContext(IdContext);
+  const {setDestinationCountry, setDepartureCountry} = useContext(IdContext);
   const history = useHistory();
 
   //convert input to placeID to send it to ApiData
   const convertDeparture = () => {
     return departureApi(departureInput)
       .then((res) => {
-        if (res.Places) {
-          setDepartureId(res.Places.map((airport) => airport.PlaceId));
-          setArrivalInfo(res.Places.map((airport) => airport));
-          setDepartureName(res.Places.map((airport) => airport.PlaceName));
-          setDepartureCountry(res.Places.map((airport) => airport.CountryName));
-        }
+        setDepartureId(res.Places.map((airport) => airport.PlaceId));
+        setArrivalInfo(res.Places.map((airport) => airport));
+        setDepartureName(res.Places.map((airport) => airport.PlaceName));
+        setDepartureCountry(res.Places.map((airport) => airport.CountryName))
+        return res.Places.map((airport) => airport.PlaceName);
       })
       .catch((err) => {
         console.log(err);
@@ -37,12 +36,11 @@ function FrontPage() {
   const convertDestination = () => {
     return destinationApi(destinationInput)
       .then((res) => {
-        if (res.Places) {
-          setDestinationId(res.Places.map((a) => a.PlaceId));
-          setDestinationInfo(res.Places.map((airport) => airport));
-          setDestinationName(res.Places.map((airport) => airport.PlaceName));
-          setDestinationCountry(res.Places.map((airport) => airport.CountryName))
-        }
+        setDestinationId(res.Places.map((a) => a.PlaceId));
+        setDestinationInfo(res.Places.map((airport) => airport));
+        setDestinationName(res.Places.map((airport) => airport.PlaceName));
+        setDestinationCountry(res.Places.map((airport) => airport.CountryName))
+        return res.Places.map((airport) => airport.PlaceName);
       })
       .catch((err) => {
         console.log(err);
@@ -73,35 +71,33 @@ function FrontPage() {
     const destinationName = await convertDestination();
     console.log(departureName);
     console.log(destinationName);
-
-    if (destinationName === undefined || departureName === undefined) {
+    
+    if(destinationName===undefined||departureName===undefined){
       history.push("/NoResult")
     }
-
-    else if (destinationName.length > 1 || departureName.length > 1) {
-      history.push("/MultipleChoicePage")
-    }
-    
+    else if(destinationName.length > 1 || departureName.length > 1){
+      history.push("/MultipleChoicePage")}
     else {
-      history.push("/Main")
-    };
-
-  
+      history.push("/Main")};
+    
+    // destinationName.length > 1 || departureName.length > 1
+    //   ? history.push("/MultipleChoicePage")
+    //   : history.push("/Main");
   };
 
   return (
     <>
-      {isLoading === false ?
-        <FrontPageMain>
-          <div>
-            <FrontPageH2>Where to Next?</FrontPageH2>
-            <SearchBar onChange={handleChange} onChange2={handleChange2} />
-            <SearchFlightButton onClick={handleSubmit}>
-              Search Flights
+    {isLoading===false?
+    <FrontPageMain>
+      <div>
+        <FrontPageH2>Where to Next?</FrontPageH2>
+        <SearchBar onChange={handleChange} onChange2={handleChange2} />
+        <SearchFlightButton onClick={handleSubmit}>
+          Search Flights
         </SearchFlightButton>
-          </div>
-        </FrontPageMain>
-        : <LoadingScreenDiv><LoadingPage /></LoadingScreenDiv>
+      </div>
+    </FrontPageMain>
+      :<LoadingScreenDiv><LoadingPage/></LoadingScreenDiv>
       }
     </>
   );
